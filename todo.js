2031c517a16aba5e73f6994dd66efd07439ba9cb
George <cortes.george93@icloud.com>
2024-02-03 21:52:54 -0800

    todoStructure and todo class

---
+++ todo.js
@@ -0,0 +1,16 @@
+class todo{
+    todo(input){
+        ischecked = false;
+        this.input =  input;
+    }
+
+    // abstracts todo list object creation puts todo object on dom
+    generateTodo(parent, input){
+
+    }
+    // returns a todo list object for the dom
+    todoContainer(){
+
+    }
+    
+}
