a8fb9bf54c35fbf6451b8a2517fa902ca5264e3c
George <cortes.george93@icloud.com>
2024-02-04 19:54:11 -0800

    Constructor and functions

--- todo.js
+++ todo.js
@@ -1,16 +1,28 @@
-class todo{
-    todo(input){
+export default class Todo{
+    constructor(){
+        console.log("working")
+    }
+    constructor(parent, input){
+        this.parent = parent;
         ischecked = false;
         this.input =  input;
+        this.generateTodo();
     }
 
     // abstracts todo list object creation puts todo object on dom
-    generateTodo(parent, input){
-
+    generateTodo( ){
+        this.parent.appendChild(this.todoContainer());
     }
     // returns a todo list object for the dom
     todoContainer(){
-
+        let div = document.createElement("div");
+        let title = document.createElement("h2");
+        let description = document.createElement("p");
+        title.innerText= `${this.input}`;
+        div.appendChild(title);
+        return div;
     }
-    
-}
+
+};
+
+
